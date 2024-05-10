8a2506ca53ae237f41b53bf440f364bf18b77380
AthosAzevedo <athostan99@gmail.com>
2024-05-10 16:07:14 -0300

    update

--- src/FACULDADE/AlunoPosGraduacao.java
+++ src/FACULDADE/AlunoPosGraduacao.java
@@ -1,7 +1,7 @@
 package FACULDADE;
 
 public class AlunoPosGraduacao extends AlunoGraduacao {
-    public AlunoPosGraduacao(String nome, String endereco, String matricula, Curso curso) {
+    public AlunoPosGraduacao(String nome, String endereco, String matricula, Curso curso, Professor orientador) {
         super(nome, endereco, matricula, curso);
     }
 
@@ -9,4 +9,11 @@ public class AlunoPosGraduacao extends AlunoGraduacao {
     public void calcularMedia() {
         super.calcularMedia();
     }
+
+    public void escreverTese(String tema, Professor orientador){
+        System.out.println(this.getNome()+" escreveu sua tese sobre "+"''"+tema+"''"+" com sucesso, sob a " +
+                "orientação de "+orientador.getNome());
+    }
+
+
 }
--- src/FACULDADE/Main.java
+++ src/FACULDADE/Main.java
@@ -12,18 +12,46 @@ public class Main {
         List<String> listasecundaria = new ArrayList<>();
         listasecundaria.add("1");
         listasecundaria.add("2");
+
+        List<String> disciplinaObrigatoria = new ArrayList<>();
+        disciplinaObrigatoria.add("Calculo 1");
+        disciplinaObrigatoria.add("Logica em Programção");
+        disciplinaObrigatoria.add("Progamção Orientada a Gambiarra");
+        List<String> disciplinaEletiva = new ArrayList<>();
+        disciplinaEletiva.add("Meio Ambiente");
+        disciplinaEletiva.add("Robotica");
+
+
 //
-        Curso c = new Curso("Eng","205555", listaInicial,listasecundaria);
+        Curso c = new Curso("102030","Engenharia",disciplinaObrigatoria,disciplinaEletiva);
 //        Aluno a = new Aluno("Athos","Fsa","2132",c);
+        c.apresentar();
 //
 //        System.out.println(c.getDisciplinaObrigatorias()+"\n"+c.getDisciplinaEletivas());
-//        Professor p = new Professor("kiko","feira","artes",listasecundaria);
-//        Disciplina d = new Disciplina("Matematica","oaoa",20,p);
+        Professor p = new Professor("kiko","feira","artes",disciplinaEletiva);
+
+        Disciplina d[] = new Disciplina[5];
+        d[0] = new Disciplina("Logica em Progamação","0101110",36,p);
+        d[1] = new Disciplina("Progamação Orientada a Objetos","1010110",32,p);
+        d[2] = new Disciplina("Calculo 1","0110011",36,p);
+        d[3] = new Disciplina("Matématica Aplicada","01001010",36,p);
+        d[4] = new Disciplina("Algebra Linear","0110101010",36,p);
+
+
+        p.adicionarDisciplina(d[0]);
+        p.adicionarDisciplina(d[2]);
+        p.adicionarDisciplina(d[3]);
+        p.apresentar();
+
+
 
 //        p.adicionarDisciplina(d);
-//        System.out.println(p.getDisciplinasMinistradas());
 
-        Aluno a = new Aluno("Athos","Feira","2132",c);
-        a.calcularMedia();
+
+//        Aluno a = new Aluno("Athos","Feira","2132",c);
+////        a.calcularMedia();
+//
+//        AlunoPosGraduacao ap = new AlunoPosGraduacao("Jeca","Feira de Santana","29io",c,p);
+//        ap.escreverTese("Inteligencia Artificial",p);
     }
 }
